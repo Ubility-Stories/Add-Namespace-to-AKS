ed2906508f5024bf2d5a72a61c46c0c01792bc56
ibrahim nasri <ibrahimnasri24@gmail.com>
2023-08-02 11:07:42 +0300

    Created repo

---
+++ main.tf
@@ -0,0 +1,23 @@
+data "azurerm_resource_group" "main" {
+  name     = var.resource-group-name
+}
+
+data "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks-name
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
+
+resource "null_resource" "get_creds" {
+  depends_on = [data.azurerm_kubernetes_cluster.aks]
+  provisioner "local-exec" {
+    command = "az aks get-credentials --resource-group ${data.azurerm_resource_group.main.name} --name ${data.azurerm_kubernetes_cluster.aks.name} --overwrite-existing"
+  }
+}
+
+resource "null_resource" "kubectl_apply" {
+  depends_on = [data.azurerm_kubernetes_cluster.aks]
+  provisioner "local-exec" {
+    command = "kubectl create ns new"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "resource-group-name" {
+    type = string
+    default = "test"
+  
+}
+variable "aks-name" {
+    type = string
+    default = "deployment"
+  
+}
